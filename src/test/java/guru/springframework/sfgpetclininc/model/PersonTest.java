ec841660d07d11e4200aafd6df0f78edefb557ee
johnson.chow <johnson.chow@fda.hhs.gov>
2020-07-20 14:27:18 -0400

    Sample Codes for JUnit 5

--- src/test/java/guru/springframework/sfgpetclininc/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclininc/model/PersonTest.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgpetclininc.model;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import guru.springframework.sfgpetclinic.model.Person;
 
@@ -17,6 +19,13 @@ public class PersonTest {
             () -> assertEquals("Johnson", person.getFirstName()),
             () -> assertEquals("Chow", person.getLastName(), "Field: Last Name Not Equal")
         );
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestsWithDI(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+
+        testInfo.
     }
     
 }
